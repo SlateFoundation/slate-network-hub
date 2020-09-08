be44874bf32b71023423489e705921f0082320bb
Nafis Bey <nafis@jarv.us>
2020-09-08 13:54:58 +0000

    fix: User model
    - add SchoolNumber column
    - add Schools relationship

--- php-classes/Slate/NetworkHub/User.php
+++ php-classes/Slate/NetworkHub/User.php
@@ -4,5 +4,17 @@ namespace Slate\NetworkHub;
 
 class User extends \Emergence\People\User
 {
-    // todo: deprecate?
+    public static $fields = [
+        'SchoolNumber'
+    ];
+
+    public static $relationships = [
+        'Schools' => [
+            'class' => School::class,
+            'type' => 'one-many',
+            'linkClass' => SchoolUser::class,
+            'linkLocal' => 'PersonID',
+            'linkForeign' => 'SchoolID'
+        ]
+    ];
 }
